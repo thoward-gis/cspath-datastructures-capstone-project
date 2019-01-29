d355b73b84ebd0113350e3d3036a23f1af6f5779
thoward-gis <47043844+thoward-gis@users.noreply.github.com>
2019-01-29 11:33:05 -0700

    Update trie.py
    Removed main()

--- Unit_8_Capstone/trie.py
+++ Unit_8_Capstone/trie.py
@@ -3,19 +3,11 @@ class Trie:
     self.value = value
     self.children = {}
 
-##  def __repr__(self, level=0):
-##    # HELPER METHOD TO PRINT TREE!
-##    ret = "--->" * level + repr(self.value) + "\n"
-##    for child in self.children.items():
-##      ret += child.__repr__(level+1)
-##    return ret
-
-  def print_this(self, level=0):
+  def __repr__(self, level=0):
     # HELPER METHOD TO PRINT TREE!
-    ret = "--->" * level + str(self.value) + "\n"
+    ret = "--->" * level + repr(self.value) + "\n"
     for child, child_node in self.children.items():
-      ret += child_node.print_this(level+1)
-    #print(ret)
+      ret += child_node.__repr__(level+1)
     return ret
 
   def add_child(self, child_value):
@@ -68,12 +60,3 @@ class Trie:
         return False
     #print(str(len(parent.children)) + " results found for input!")
     return [prefix + suffix for suffix in self.list_words(parent)]
-
-def main():
-    string_list = ["chance", "charge", "choice", "chore", "cory", "abc", "aids"]
-    test = Trie()
-    print(test.print_this())
-    test.add_list_values(string_list)
-    print(test.print_this())
-    print(test.list_words(test))
-    print(test.return_possibilities("ch"))
